357744922b786769f220d9b03508817b4ed9a903
VERIVOX\VarennikS <semen.varennik@verivox.com>
2021-01-17 08:22:53 +0300

    -

--- electrical-wiring.js
+++ electrical-wiring.js
@@ -1,4 +1,30 @@
 Draw.loadPlugin(function(ui) {
     var graph = ui.editor.graph;
-    debugger;
+    
+    function cellClicked(cell)
+	{
+		// Forces focusout in IE
+		graph.container.focus();
+
+        debugger;
+
+		// Gets the selection cell
+		if (cell == null)
+		{
+			
+		}
+		else
+		{
+			
+		}
+	};
+
+    graph.click = function(me)
+	{
+		// Async required to enable hyperlinks in labels
+		window.setTimeout(function()
+		{
+			cellClicked(me.getCell());
+		}, 0);
+	};
 });
